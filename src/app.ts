8bc6e3cf516ed71a4fe55f33e0629e673fa330b5
NITIN-BINDAL <nitinbindal100@gmail.com>
2020-07-22 16:11:51 +0530

    Added Swagger

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Response, Application } from 'express';
+import express, { Response, Application, NextFunction } from 'express';
 import mongoose from 'mongoose';
 
 import passport from 'passport';
@@ -7,10 +7,25 @@ import config from './config';
 
 import AuthRouter from './routers/auth.router';
 
+import swaggerui from 'swagger-ui-express';
+
+import swaggerJSDoc from 'swagger-jsdoc';
+
+import def from './definitions.js';
+
+const options = {
+	definition: def,
+
+	apis: ['./routers/auth.router.ts']
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
 const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
+app.get('/swagger', swaggerui.serve, swaggerui.setup(swaggerSpec));
 
 app.listen(config.PORT, (err: any) => {
 	if (err) {
@@ -36,6 +51,20 @@ mongoose.connection.on(
 app.use(function (err: any, res: Response) {
 	res.status(err.status || 500);
 	res.end(err.message);
+});
+
+app.use(function (req: any, res: Response, next: NextFunction) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, UPDATE, DELETE, OPTIONS'
+	);
+	res.header('Content-Type', 'application/json');
+	next();
 });
 
 module.exports = app;
