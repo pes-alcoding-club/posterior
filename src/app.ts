44d8c6ffceb81c2818c95d5b31e50d29fae6840d
NITIN-BINDAL <nitinbindal100@gmail.com>
2020-07-22 12:36:37 +0530

    Updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,28 @@ import config from './config';
 
 import AuthRouter from './routers/auth.router';
 
+import swaggerui from 'swagger-ui-express';
+
+import swaggerJSDoc from 'swagger-jsdoc';
+
+import def from './definitions.js';
+
+const options ={
+
+definition:def,
+
+apis: ['./routers/auth.router.ts']
+
+}
+
+const swaggerSpec = swaggerJSDoc(options);
+
 const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
+app.get('/swagger',swaggerui.serve,swaggerui.setup(swaggerSpec));
+
 
 app.listen(config.PORT, (err: any) => {
 	if (err) {
@@ -37,5 +55,15 @@ app.use(function (err: any, res: Response) {
 	res.status(err.status || 500);
 	res.end(err.message);
 });
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, UPDATE, DELETE, OPTIONS')
+   res.header("Content-Type", "application/json");
+   next();
+ });
+
+
 
 module.exports = app;
