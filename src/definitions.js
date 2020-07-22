3372cf4bc64e7ea1ec0551cb25c19935a6a0679f
NITIN-BINDAL <nitinbindal100@gmail.com>
2020-07-22 12:38:00 +0530

    Added definitions.js

---
+++ src/definitions.js
@@ -0,0 +1,17 @@
+module.exports = {
+    info: {
+   
+      title: 'The Alcoding Club Application Api\'s',
+      version: '1.0.0',
+      description: 'All Backend Api\'s end points', 
+    },
+    host:'localhost:3000', // Host 
+    basePath: '/', // Base path 
+    schemes: ['http'],
+    consumes: [
+        "application/x-www-form-urlencoded"
+      ],
+    produces: [
+        "application/json"
+      ]
+  };
